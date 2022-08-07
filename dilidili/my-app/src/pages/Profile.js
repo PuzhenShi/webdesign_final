bbc591d2a7ca14eac8fd6f94e045fa07f25fe5cf
PuzhenShi <shi.pu@northeastern.edu>
2022-08-07 19:40:44 -0400

    Update Profile.js

--- dilidili/my-app/src/pages/Profile.js
+++ dilidili/my-app/src/pages/Profile.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Outlet, IndexRoute, Routes, Route, Link } from 'react-router-dom'
 import ProfileList from '../components/Profile/ProfileList'
-import ProfileHome from '../components/Profile/ProfileHome'
-import ProfileHead from '../components/Profile/ProfileHead'
-import ProfileInfo from '../components/Profile/ProfileInfo'
-import ProfileLogin from '../components/Profile/ProfileLogin'
+
 //the profile page
 function Profile(props) {
     // const match = props.match;
