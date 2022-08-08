c8ee25497c355af414b74a2a8e0b18a526c8977a
xinwei-12 <cao.xinw@northeastern.edu>
2022-08-08 01:18:09 -0400

    Connect to Database
    Connect to mongodb Database

---
+++ Server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+
+
+//connect to mongoose
+mongoose.connect("mongodb+srv://Team2:team2Final@info6150-final.bnqj3iv.mongodb.net/?retryWrites=true&w=majority");
+
+//require routes
+app.use("/users/", require("./routes/userRoute"));
+app.use("/videodb/", require("./routes/videoRoute"));
+
+app.listen(3001, function(){
+    console.log(">>> express is running");
+});
