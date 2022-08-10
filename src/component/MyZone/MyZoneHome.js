b574a685d477c632e3a8166d1ae00f8582c9db6e
PuzhenShi <shi.pu@northeastern.edu>
2022-08-10 13:48:26 -0400

    Update MyZoneHome.js

--- src/component/MyZone/MyZoneHome.js
+++ src/component/MyZone/MyZoneHome.js
@@ -83,333 +83,333 @@ function MyZoneHome() {
 
   //console.log(userFind);
 
-//display myfavorite 
-let myFavoritelist = [];
+  //display myfavorite 
+  let myFavoritelist = [];
 
-if(userFind.myFavorite){
-  myFavoritelist=userFind.myFavorite;
-}
-  
-
-let myFavorite = (myFavoritelist) =>{
-  if(myFavoritelist.length==0){
-    return(
-    <div>
-  
-  You don't have any liked video!
-
-    </div>
-    )
-  } else if (myFavoritelist.length==1){
-
-    let videof1 = myFavoritelist[myFavoritelist.length - 1];
-
-    fvideo1 = {
-      url: videof1.videoAddress,
-      title: videof1.videoName,
-      duration: videof1.videoDuration,
-      views: videof1.NOC,
-      cover: videof1.videoCover,
-      author: videof1.publisher,
-      author_id: 1,
-      date: videof1.uploadTime,
-      VIP: videof1.VIP,
-    };
-
-    return(
-      <div class="col-4">
-                <VideoCover
-                  videoInfo={fvideo1}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-    )
-  } else if(myFavoritelist.length===2){
-    let videof1 = myFavoritelist[myFavoritelist.length - 1];
-    let videof2 = myFavoritelist[myFavoritelist.length - 2];
-    fvideo1 = {
-      url: videof1.videoAddress,
-      title: videof1.videoName,
-      duration: videof1.videoDuration,
-      views: videof1.NOC,
-      cover: videof1.videoCover,
-      author: videof1.publisher,
-      author_id: 1,
-      date: videof1.uploadTime,
-      VIP: videof1.VIP,
-    };
-    fvideo2 = {
-      url: videof2.videoAddress,
-      title: videof2.videoName,
-      duration: videof2.videoDuration,
-      views: videof2.NOC,
-      cover: videof2.videoCover,
-      author: videof2.publisher,
-      author_id: 1,
-      date: videof2.uploadTime,
-      VIP: videof2.VIP,
-    };
-
-    return(
-      <div class="col-12 row" id="myZoneFavoriteGallery">
-      <div class="col-4">
-                <VideoCover
-                  videoInfo={fvideo1}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-      <div class="col-4">
-      <VideoCover
-        videoInfo={fvideo2}
-        coverWidth={200}
-        coverHeight={120}
-        width={240}
-      ></VideoCover>
-    </div>
-    </div>
-    )
-  }else{
-    let videof1 = myFavoritelist[myFavoritelist.length - 1];
-    let videof2 = myFavoritelist[myFavoritelist.length - 2];
-    let videof3 = myFavoritelist[myFavoritelist.length - 3];
-    fvideo1 = {
-      url: videof1.videoAddress,
-      title: videof1.videoName,
-      duration: videof1.videoDuration,
-      views: videof1.NOC,
-      cover: videof1.videoCover,
-      author: videof1.publisher,
-      author_id: 1,
-      date: videof1.uploadTime,
-      VIP: videof1.VIP,
-    };
-    fvideo2 = {
-      url: videof2.videoAddress,
-      title: videof2.videoName,
-      duration: videof2.videoDuration,
-      views: videof2.NOC,
-      cover: videof2.videoCover,
-      author: videof2.publisher,
-      author_id: 1,
-      date: videof2.uploadTime,
-      VIP: videof2.VIP,
-    };
-    fvideo3 = {
-      url: videof3.videoAddress,
-      title: videof3.videoName,
-      duration: videof3.videoDuration,
-      views: videof3.NOC,
-      cover: videof3.videoCover,
-      author: videof3.publisher,
-      author_id: 1,
-      date: videof3.uploadTime,
-      VIP: videof3.VIP,
-    };
-
-    return(
-      <div class="col-12 row" id="myZoneFavoriteGallery">
-        <div class="col-4">
-                <VideoCover
-                  videoInfo={fvideo1}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-              <div class="col-4">
-                <VideoCover
-                  videoInfo={fvideo2}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-              <div class="col-4">
-                <VideoCover
-                  videoInfo={fvideo3}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-      </div>
-    )
-
+  if (userFind.myFavorite) {
+    myFavoritelist = userFind.myFavorite;
   }
 
 
-}
+  let myFavorite = (myFavoritelist) => {
+    if (myFavoritelist.length == 0) {
+      return (
+        <div>
+
+          You don't have any liked video!
+
+        </div>
+      )
+    } else if (myFavoritelist.length == 1) {
+
+      let videof1 = myFavoritelist[myFavoritelist.length - 1];
+
+      fvideo1 = {
+        url: videof1.videoAddress,
+        title: videof1.videoName,
+        duration: videof1.videoDuration,
+        views: videof1.NOC,
+        cover: videof1.videoCover,
+        author: videof1.publisher,
+        author_id: 1,
+        date: videof1.uploadTime,
+        VIP: videof1.VIP,
+      };
+
+      return (
+        <div class="col-4">
+          <VideoCover
+            videoInfo={fvideo1}
+            coverWidth={200}
+            coverHeight={120}
+            width={240}
+          ></VideoCover>
+        </div>
+      )
+    } else if (myFavoritelist.length === 2) {
+      let videof1 = myFavoritelist[myFavoritelist.length - 1];
+      let videof2 = myFavoritelist[myFavoritelist.length - 2];
+      fvideo1 = {
+        url: videof1.videoAddress,
+        title: videof1.videoName,
+        duration: videof1.videoDuration,
+        views: videof1.NOC,
+        cover: videof1.videoCover,
+        author: videof1.publisher,
+        author_id: 1,
+        date: videof1.uploadTime,
+        VIP: videof1.VIP,
+      };
+      fvideo2 = {
+        url: videof2.videoAddress,
+        title: videof2.videoName,
+        duration: videof2.videoDuration,
+        views: videof2.NOC,
+        cover: videof2.videoCover,
+        author: videof2.publisher,
+        author_id: 1,
+        date: videof2.uploadTime,
+        VIP: videof2.VIP,
+      };
+
+      return (
+        <div class="col-12 row" id="myZoneFavoriteGallery">
+          <div class="col-4">
+            <VideoCover
+              videoInfo={fvideo1}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+          <div class="col-4">
+            <VideoCover
+              videoInfo={fvideo2}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+        </div>
+      )
+    } else {
+      let videof1 = myFavoritelist[myFavoritelist.length - 1];
+      let videof2 = myFavoritelist[myFavoritelist.length - 2];
+      let videof3 = myFavoritelist[myFavoritelist.length - 3];
+      fvideo1 = {
+        url: videof1.videoAddress,
+        title: videof1.videoName,
+        duration: videof1.videoDuration,
+        views: videof1.NOC,
+        cover: videof1.videoCover,
+        author: videof1.publisher,
+        author_id: 1,
+        date: videof1.uploadTime,
+        VIP: videof1.VIP,
+      };
+      fvideo2 = {
+        url: videof2.videoAddress,
+        title: videof2.videoName,
+        duration: videof2.videoDuration,
+        views: videof2.NOC,
+        cover: videof2.videoCover,
+        author: videof2.publisher,
+        author_id: 1,
+        date: videof2.uploadTime,
+        VIP: videof2.VIP,
+      };
+      fvideo3 = {
+        url: videof3.videoAddress,
+        title: videof3.videoName,
+        duration: videof3.videoDuration,
+        views: videof3.NOC,
+        cover: videof3.videoCover,
+        author: videof3.publisher,
+        author_id: 1,
+        date: videof3.uploadTime,
+        VIP: videof3.VIP,
+      };
+
+      return (
+        <div class="col-12 row" id="myZoneFavoriteGallery">
+          <div class="col-4">
+            <VideoCover
+              videoInfo={fvideo1}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+          <div class="col-4">
+            <VideoCover
+              videoInfo={fvideo2}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+          <div class="col-4">
+            <VideoCover
+              videoInfo={fvideo3}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+        </div>
+      )
+
+    }
+
+
+  }
   let fvideo1 = {};
   let fvideo2 = {};
   let fvideo3 = {};
 
   //display mylist 
-  let myvideo=[];
-  if(userFind.myVideo){
+  let myvideo = [];
+  if (userFind.myVideo) {
     myvideo = userFind.myVideo;
   }
-console.log(userFind);
+  console.log(userFind);
 
-let myvideolist = (myvideo) =>{
-  if(myvideo.length==0){
-    return(
-    <div>
-  
-    You didn't uplaod any video!
+  let myvideolist = (myvideo) => {
+    if (myvideo.length == 0) {
+      return (
+        <div>
 
-    </div>
-    )
-  } else if (myvideo.length==1){
+          You didn't uplaod any video!
 
-    let videof1 = myvideo[myvideo.length - 1];
+        </div>
+      )
+    } else if (myvideo.length == 1) {
 
-    lvideo1 = {
-      url: videof1.videoAddress,
-      title: videof1.videoName,
-      duration: videof1.videoDuration,
-      views: videof1.NOC,
-      cover: videof1.videoCover,
-      author: videof1.publisher,
-      author_id: 1,
-      date: videof1.uploadTime,
-      VIP: videof1.VIP,
-    };
+      let videof1 = myvideo[myvideo.length - 1];
 
-    return(
-      <div class="col-4">
-                <VideoCover
-                  videoInfo={lvideo1}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-    )
-  } else if(myvideo.length===2){
-    let videof1 = myvideo[myvideo.length - 1];
-    let videof2 = myvideo[myvideo.length - 2];
-    lvideo1 = {
-      url: videof1.videoAddress,
-      title: videof1.videoName,
-      duration: videof1.videoDuration,
-      views: videof1.NOC,
-      cover: videof1.videoCover,
-      author: videof1.publisher,
-      author_id: 1,
-      date: videof1.uploadTime,
-      VIP: videof1.VIP,
-    };
-    lvideo2 = {
-      url: videof2.videoAddress,
-      title: videof2.videoName,
-      duration: videof2.videoDuration,
-      views: videof2.NOC,
-      cover: videof2.videoCover,
-      author: videof2.publisher,
-      author_id: 1,
-      date: videof2.uploadTime,
-      VIP: videof2.VIP,
-    };
+      lvideo1 = {
+        url: videof1.videoAddress,
+        title: videof1.videoName,
+        duration: videof1.videoDuration,
+        views: videof1.NOC,
+        cover: videof1.videoCover,
+        author: videof1.publisher,
+        author_id: 1,
+        date: videof1.uploadTime,
+        VIP: videof1.VIP,
+      };
 
-    return(
-      <div class="col-12 row" id="myZoneVediosGallery">
-      <div class="col-4">
-                <VideoCover
-                  videoInfo={lvideo1}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-      <div class="col-4">
-      <VideoCover
-        videoInfo={lvideo2}
-        coverWidth={200}
-        coverHeight={120}
-        width={240}
-      ></VideoCover>
-    </div>
-    </div>
-    )
-  }else{
-    let videof1 = myvideo[myvideo.length - 1];
-    let videof2 = myvideo[myvideo.length - 2];
-    let videof3 = myvideo[myvideo.length - 3];
-    lvideo1 = {
-      url: videof1.videoAddress,
-      title: videof1.videoName,
-      duration: videof1.videoDuration,
-      views: videof1.NOC,
-      cover: videof1.videoCover,
-      author: videof1.publisher,
-      author_id: 1,
-      date: videof1.uploadTime,
-      VIP: videof1.VIP,
-    };
-    lvideo2 = {
-      url: videof2.videoAddress,
-      title: videof2.videoName,
-      duration: videof2.videoDuration,
-      views: videof2.NOC,
-      cover: videof2.videoCover,
-      author: videof2.publisher,
-      author_id: 1,
-      date: videof2.uploadTime,
-      VIP: videof2.VIP,
-    };
-    lvideo3 = {
-      url: videof3.videoAddress,
-      title: videof3.videoName,
-      duration: videof3.videoDuration,
-      views: videof3.NOC,
-      cover: videof3.videoCover,
-      author: videof3.publisher,
-      author_id: 1,
-      date: videof3.uploadTime,
-      VIP: videof3.VIP,
-    };
-
-    return(
-      <div class="col-12 row" id="myZoneVediosGallery">
+      return (
         <div class="col-4">
-                <VideoCover
-                  videoInfo={lvideo1}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-              <div class="col-4">
-                <VideoCover
-                  videoInfo={lvideo2}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-              <div class="col-4">
-                <VideoCover
-                  videoInfo={lvideo3}
-                  coverWidth={200}
-                  coverHeight={120}
-                  width={240}
-                ></VideoCover>
-              </div>
-      </div>
-    )
+          <VideoCover
+            videoInfo={lvideo1}
+            coverWidth={200}
+            coverHeight={120}
+            width={240}
+          ></VideoCover>
+        </div>
+      )
+    } else if (myvideo.length === 2) {
+      let videof1 = myvideo[myvideo.length - 1];
+      let videof2 = myvideo[myvideo.length - 2];
+      lvideo1 = {
+        url: videof1.videoAddress,
+        title: videof1.videoName,
+        duration: videof1.videoDuration,
+        views: videof1.NOC,
+        cover: videof1.videoCover,
+        author: videof1.publisher,
+        author_id: 1,
+        date: videof1.uploadTime,
+        VIP: videof1.VIP,
+      };
+      lvideo2 = {
+        url: videof2.videoAddress,
+        title: videof2.videoName,
+        duration: videof2.videoDuration,
+        views: videof2.NOC,
+        cover: videof2.videoCover,
+        author: videof2.publisher,
+        author_id: 1,
+        date: videof2.uploadTime,
+        VIP: videof2.VIP,
+      };
+
+      return (
+        <div class="col-12 row" id="myZoneVediosGallery">
+          <div class="col-4">
+            <VideoCover
+              videoInfo={lvideo1}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+          <div class="col-4">
+            <VideoCover
+              videoInfo={lvideo2}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+        </div>
+      )
+    } else {
+      let videof1 = myvideo[myvideo.length - 1];
+      let videof2 = myvideo[myvideo.length - 2];
+      let videof3 = myvideo[myvideo.length - 3];
+      lvideo1 = {
+        url: videof1.videoAddress,
+        title: videof1.videoName,
+        duration: videof1.videoDuration,
+        views: videof1.NOC,
+        cover: videof1.videoCover,
+        author: videof1.publisher,
+        author_id: 1,
+        date: videof1.uploadTime,
+        VIP: videof1.VIP,
+      };
+      lvideo2 = {
+        url: videof2.videoAddress,
+        title: videof2.videoName,
+        duration: videof2.videoDuration,
+        views: videof2.NOC,
+        cover: videof2.videoCover,
+        author: videof2.publisher,
+        author_id: 1,
+        date: videof2.uploadTime,
+        VIP: videof2.VIP,
+      };
+      lvideo3 = {
+        url: videof3.videoAddress,
+        title: videof3.videoName,
+        duration: videof3.videoDuration,
+        views: videof3.NOC,
+        cover: videof3.videoCover,
+        author: videof3.publisher,
+        author_id: 1,
+        date: videof3.uploadTime,
+        VIP: videof3.VIP,
+      };
+
+      return (
+        <div class="col-12 row" id="myZoneVediosGallery">
+          <div class="col-4">
+            <VideoCover
+              videoInfo={lvideo1}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+          <div class="col-4">
+            <VideoCover
+              videoInfo={lvideo2}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+          <div class="col-4">
+            <VideoCover
+              videoInfo={lvideo3}
+              coverWidth={200}
+              coverHeight={120}
+              width={240}
+            ></VideoCover>
+          </div>
+        </div>
+      )
+
+    }
+
 
   }
-
-
-}
   let lvideo1 = {};
   let lvideo2 = {};
   let lvideo3 = {};
-  
+
 
 
 
@@ -426,15 +426,15 @@ let myvideolist = (myvideo) =>{
   return (
     <div class="container">
       <div class="col-12 row" id="myZonePanel">
-        <div class="col-10 rounded">
+        <div class="col-12 rounded">
           {/* first eight vedio(my vedio) display card will be posted here */}
           <div class="col-12 rounded" id="myZoneVedios">
-            <span>My Vedios</span>
-            <Link to="/myzone/like">
-              <Button variant="primary">
-                More
-              </Button>
-            </Link>
+            <div class="col-12 rounded" id="myZoneVideosBar">
+              <span>My Vedios</span>
+              <Link to="/myzone/videos" id="myVideosLink" >                
+                  More>>>
+              </Link>
+            </div>
 
             <div class="col-12 row" id="myZoneVediosGallery">
               {myvideolist(myvideo)}
@@ -442,14 +442,17 @@ let myvideolist = (myvideo) =>{
 
             </div>
           </div>
+
+
+
           {/* first eight vedio(favorite) display card will be posted here */}
           <div class="col-12 rounded" id="myZoneFavorite">
-            <span>My Favorite</span>
-            <Link to="/myzone/favorite">
-              <Button variant="primary">
-                More
-              </Button>
-            </Link>
+            <div class="col-12 rounded" id="myZoneFavoriteBar">
+              <span>My Favorite</span>
+              <Link to="/myzone/favorite" id="myFavoriteLink" >                
+                  More>>>
+              </Link>
+            </div>
 
             <div class="col-12 row" id="myZoneFavoriteGallery">
               {myFavorite(myFavoritelist)}
