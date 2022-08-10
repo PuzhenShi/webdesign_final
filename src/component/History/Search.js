a2d5fcac8305e1ddfa11c957b648b6ef478c6b9e
ShunQ Li <li.shunq@northeastern.edu>
2022-08-10 09:00:44 -0400

    upload

---
+++ src/component/History/Search.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Scroll from './Scroll';
+import SearchList from './SearchList';
+
+function Search({ details }) {
+
+  const [searchField, setSearchField] = useState("");
+
+  const filteredVideos = details.filter(
+    video => {
+      return (
+        video
+        .videoName
+        .toLowerCase()
+        .includes(searchField.toLowerCase())
+      );
+    }
+  );
+
+  const handleChange = e => {
+    setSearchField(e.target.value);
+  };
+
+  function searchList() {
+    return (
+      <Scroll>
+        <SearchList filteredVideos={filteredVideos} />
+      </Scroll>
+    );
+  }
+
+  return (
+    <section className="garamond">
+      <div className="navy georgia ma0 grow">
+        <h2 className="f2">Search video</h2>
+      </div>
+      <div className="pa2">
+        <input 
+          className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+          type = "search" 
+          placeholder = "Search Video" 
+          onChange = {handleChange}
+        />
+      </div>
+      {searchList()}
+    </section>
+  );
+}
+
+export default Search;
